8d57bd5ce88e24b4243cc2eb10a0741af004aca0
Achyutdhaduk <achyutdhaduk8139@gmail.com>
2024-07-15 10:29:52 +0530

    javascript add

---
+++ 07reactrouter/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+import Footer from './component/Footer/Footer';
+import Header from './component/Header/Header';
+import Home from './component/Home/Home.JSX';
+
+function App() {
+  return (
+    
+   <>
+
+
+   <Header></Header>
+   <Home/>
+   <Footer></Footer>
+  
+    
+    <h1 className='bg-green-600'>React router</h1>
+   </>
+  );
+}
+
+export default App;
