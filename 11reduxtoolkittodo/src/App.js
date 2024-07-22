e12eb7d326cb264ebd350ed46f76531f8bd3e69f
Achyutdhaduk <achyutdhaduk8139@gmail.com>
2024-07-22 22:15:40 +0530

    add functionality

---
+++ 11reduxtoolkittodo/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
+
+function App() {
+  return (
+   <>
+    <h1 class ="justify-items-center" >helloo</h1>
+    <AddTodo class ="content-center"/>
+    <Todos class ="content-center"/>
+   </>
+  );
+}
+
+export default App;
